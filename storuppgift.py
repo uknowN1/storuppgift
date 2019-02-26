2772b6bfdfdcc77b724542597edf86276d0b520e
Oscar Lindqvist <lios16@nti.umea>
2019-02-26 10:25:48 +0100

    meny

---
+++ storuppgift.py
@@ -0,0 +1,11 @@
+import os
+
+print"*** Välkommen till HA-programet ***"
+print"Gör ett val i menyn:"
+print"1. läs in fil"
+print"2. hantera fil"
+print"3. spara fil"
+print"4. avsluta"
+
+while != 4:
+    
